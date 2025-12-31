6b69ca97ea280451e002e3c80b36515ce81ddaf2
raajasimha5 <34297767+raajasimha5@users.noreply.github.com>
2025-12-31 10:54:43 +0530

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,5 +1,4 @@
-resource "google_project_iam _member" "terraform_sa_compute"
-{
+resource "google_project_iam _member" "terraform_sa_compute"{
 project = "smiling-breaker-325908"
 role    = "roles/compute.admin"
 member  = "serviceAcount:${google_service_account.terraform_sa.email}"
