1ca7fccdbb677c86350c52021ed8a0338ab1edf1
raajasimha5 <34297767+raajasimha5@users.noreply.github.com>
2026-01-05 10:31:18 +0530

    Add Google Source Repository resource to sa.tf

--- sa.tf
+++ sa.tf
@@ -1,3 +1,7 @@
+resource "google_sourcerepo_repository" "my-repo" {
+  name = "terrafrom-demo"
+project = "smiling-breaker-325908"
+}
 resource "google_project_iam _member" "terraform_sa_compute"{
 project = "smiling-breaker-325908"
 role    = "roles/compute.admin"
