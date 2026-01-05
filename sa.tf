941b7c90f13a93acd5e241f3654a99e384ff3e9d
raajasimha5 <raajasimha5@gmail.com>
2026-01-05 07:15:33 +0000

    changes added

--- sa.tf
+++
@@ -1,9 +0,0 @@
-resource "google_sourcerepo_repository" "my-repo" {
-  name = "terrafrom-demo"
-project = "smiling-breaker-325908"
-}
-resource "google_project_iam _member" "terraform_sa_compute"{
-project = "smiling-breaker-325908"
-role    = "roles/compute.admin"
-member  = "serviceAcount:${google_service_account.terraform_sa.email}"
-}
---
+++ varibale.tf
@@ -0,0 +1,47 @@
+variable "gcp_project_id" {
+  description = "The GCP project ID to deploy resources into."
+  type        = string
+  default     = "smiling-breaker-325908" # Not recommended for environment-specific values
+}
+
+variable "gcp_region" {
+  description = "The GCP region for the resources."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "gcp_zone" {
+  description = "The GCP zone for the VM instance."
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "tf_state_bucket_name" {
+  description = "The globally unique name for the GCS bucket to store Terraform state."
+  type        = string
+  default     = "test-bucket"
+}
+
+variable "vm_instance_count" {
+  description = "The number of VM instances to create."
+  type        = number
+  default     = 1
+}
+
+variable "vm_instance_name" {
+  description = "The base name for the VM instances."
+  type        = string
+  default     = "test-vm-0"
+}
+
+variable "vm_machine_type" {
+  description = "The machine type for the VM."
+  type        = string
+  default     = "e2-small"
+}
+
+variable "vm_image" {
+  description = "The boot disk image for the VM."
+  type        = string
+  default     = "debian-cloud/debian-11"
+}
