d781fe89133bce48377dc27113223fc567319656
raajasimha5 <34297767+raajasimha5@users.noreply.github.com>
2026-01-06 14:27:22 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_names" {
-  description = "The names of the created VM instances."
-  value       = google_compute_instance.default.*.name
-}
-
-output "instance_external_ips" {
-  description = "The external IP addresses of the created VM instances."
-  value       = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
-}
